5abf8d2ab8c2d4114c4729e7856b61921f79aa68
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-01-30 09:17:40 +0530

    Interpolation

---
+++ JavaScript/02-interpolation.js
@@ -0,0 +1,9 @@
+const firstName = "XYZ";
+const lastName = "ABC";
+const maritalStatus = "unmarried";
+const country="India";
+const age = 26;
+
+let details = "";
+details = `My name is ${firstName} ${lastName} and I am ${maritalStatus}. I live in ${country} and I am ${age} years old`;
+console.log(details);
