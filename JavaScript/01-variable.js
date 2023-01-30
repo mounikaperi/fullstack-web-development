c7000656e50127a62784d32a5d1e5bbc6b6bddc1
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-01-30 09:17:10 +0530

    Script

---
+++ JavaScript/01-variable.js
@@ -0,0 +1,9 @@
+const value = "John"; //String
+const isValid = false; //boolean
+let fullName; // undefined
+const firstName = null; // Null
+
+console.log(value);
+console.log(isValid);
+console.log(fullName);
+console.log(firstName);
