93499da20f20837984a18bf0cb560c33b15e57ee
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-01-31 20:57:14 +0530

    Code

---
+++ JavaScript/04-includes.js
@@ -0,0 +1,7 @@
+let text = "Hello world, welcome to the universe.";
+let result = text.includes("Script");
+console.log(result);
+
+let text1 = "hello Script";
+result = text1.includes("Script");
+console.log(result);
