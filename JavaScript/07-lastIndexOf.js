0f8382d22bb77cba9ddd1f9ad9f5f189d5fa442b
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-02-01 22:02:29 +0530

    code

---
+++ JavaScript/07-lastIndexOf.js
@@ -0,0 +1,4 @@
+let stringArray = ["Hi", "Hello", "How", "Are", "You", "pw skills", "iNeuron", "pw skills", "bye"];
+let firstIndex = stringArray.indexOf("pw skills");
+let lastIndex = stringArray.lastIndexOf("pw skills");
+console.log(`pw skills is found at position ${firstIndex} and last position at ${lastIndex}`);
