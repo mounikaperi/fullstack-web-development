7dafa94128a88aef33fc792e2cff65708a1f976e
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-02-03 20:48:01 +0530

    Code

---
+++ JavaScript/09-truthyAndFalsyValues.js
@@ -0,0 +1,36 @@
+// First one
+let enteredValue = "";
+let userName = enteredValue || "DEFAULT";
+console.log(userName); //Prints "DEFAULT"
+
+//SECOND ONE
+userName = enteredValue || null;
+console.log(userName); // Prints null
+
+// THIRD ONE
+enteredValue = 'Max';
+userName = enteredValue || 'Anna';
+console.log(userName) // Max
+
+// FOURTH ONE
+enteredValue = 'Max';
+userName = enteredValue && 'Anna';
+console.log(userName) // Anna
+
+// FIFTH ONE
+enteredValue = "";
+userName = enteredValue && "Anna";
+console.log(userName); // false
+
+// Sixth One
+enteredValue = "Max";
+userName = enteredValue && "";
+console.log(userName); // ""
+
+//Seventh One
+userName = "Max";
+console.log(!!userName); // true
+
+//Eight One
+userName = "Max";
+console.log(!userName); // false
