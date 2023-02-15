f01b6f3973df54999db585789da54cd21fc7800b
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-02-15 22:47:23 +0530

    Code

---
+++ JavaScript/21-countriesList.js
@@ -0,0 +1,6 @@
+const countries = ['India', 'Afghanistan', 'Iran', 'Iraq', 'Ireland'];
+if (countries.includes('Ethiopia')) {
+  console.log('Ethiopia'.toUpperCase());
+} else {
+  countries.push('Ethiopia');
+}
